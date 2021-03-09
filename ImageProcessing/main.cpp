734ba0f18dbbb8370e360e1de6adb6f4acd10cf3
BorisovMB <arryowtf@gmail.com>
2021-03-09 15:00:25 +0300

    delete comment in main

--- ImageProcessing/main.cpp
+++ ImageProcessing/main.cpp
@@ -3,32 +3,10 @@
 #include "gray_filter.h"
 #include "gaussian_filter.h"
 
-//std::string type2str(int type) {
-//  std::string r;
-//
-//  uchar depth = type & CV_MAT_DEPTH_MASK;
-//  uchar chans = 1 + (type >> CV_CN_SHIFT);
-//
-//  switch ( depth ) {
-//    case CV_8U:  r = "8U"; break;
-//    case CV_8S:  r = "8S"; break;
-//    case CV_16U: r = "16U"; break;
-//    case CV_16S: r = "16S"; break;
-//    case CV_32S: r = "32S"; break;
-//    case CV_32F: r = "32F"; break;
-//    case CV_64F: r = "64F"; break;
-//    default:     r = "User"; break;
-//  }
-//
-//  r += "C";
-//  r += (chans+'0');
-//
-//  return r;
-//}
 
 int main() {
-	//cv::Mat img = cv::imread("C:\\Users\\arryo\\Pictures\\unn.png");
-	cv::Mat img = cv::imread("C:\\Users\\Layzeld\\Downloads\\pic\\Annotation 2020-12-03 142825.png");
+	cv::Mat img = cv::imread("C:\\Users\\arryo\\Pictures\\unn.png");
+	//cv::Mat img = cv::imread("C:\\Users\\Layzeld\\Downloads\\pic\\Annotation 2020-12-03 142825.png");
 
     gaussian_filter fil(12, 4, img);
 	cv::Mat res_img = fil.make();
